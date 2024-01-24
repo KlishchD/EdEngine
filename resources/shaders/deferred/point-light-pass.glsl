baeaa30d5d9466b79a7e1a5ee9b11d27d8cdab5b
Dmytro Klishch <klishch.dmytro@gmail.com>
2024-01-24 10:27:55 +0200

    Refactored point light pass shader

--- resources/shaders/deferred/point-light-pass.glsl
+++ resources/shaders/deferred/point-light-pass.glsl
@@ -60,121 +60,98 @@ float GX(float dot, float r) {
     return dot / (dot * (1 - k) + k);
 }
 
-void main()
+float GetVisibility(vec2 pos, vec3 light, vec3 normal, vec3 position)
 {
-    vec2 pos = gl_FragCoord.xy / u_ScreenSize;
-    
-    vec3 albedo = texture(u_Albedo, pos).xyz;
-    vec3 position = texture(u_Position, pos).xyz;
-    vec3 normal = texture(u_Normal, pos).xyz;
-    
-    vec4 roughnessMetalic = texture(u_RoughnessMetalic, pos);
-    float roughness = roughnessMetalic.x;
-    float metalic = roughnessMetalic.y;
-    
-    if (roughnessMetalic.a == 1.0f) 
+    if (u_PointLight.UseShadowMap)
     {
-        vec3 view = normalize(u_ViewPosition - position);
-
-        float NdotV = max(dot(normal, view), 0.0f);
-
-        float r2 = roughness * roughness;
-
-        vec3 F0 = mix(vec3(0.04f), albedo, metalic);
-
-        vec3 light = normalize(u_PointLight.Position - position);
-        vec3 h = normalize(light + view);
-
-        float HdotV = max(dot(h, view), 0.0f);
-        float NdotH = max(dot(normal, h), 0.0f);
         float NdotL = max(dot(normal, light), 0.0f);
-        float LdotH = max(dot(light, h), 0.0f);
-
-        float NdotH2 = NdotH * NdotH;
-
-        vec3 F = F0 + (vec3(1.0f) - F0) * pow(clamp((1.0f - HdotV), 0.0f, 1.0f), 5.0f);
-
-        float denominator = (NdotH2 * (r2 * r2 - 1.0f) + 1.0f);
-        float D = r2 * r2 / (M_PI * denominator * denominator + 0.0001f); // r2 * r2 is Epic stuff ;)
-
-        //float t2 = (1 - NdotH2) / NdotH2;
-        //float G = GX(NdotH, LdotH, NdotL, r2, t2) * GX(NdotH, HdotV, NdotV, r2, t2);
-
-        float G = GX(NdotV, roughness) * GX(NdotL, roughness);
-
-        vec3 specular = F * G * D / (4.0f * NdotV * NdotL + 0.0001f);
-        vec3 diffuse = (vec3(1.0f) - F) * albedo / M_PI;
 
         float distance = length(u_PointLight.Position - position);
-        vec3 radiance = u_PointLight.Intensity * u_PointLight.Color / (distance * distance);
-
-        if (distance > u_PointLight.Radius) {
-            discard;
-        }
 
         float bias = max(5.0f * (1.0f - NdotL), 1.0f);
         float shadowIntensity = 0;
 
-        /*
-
-        float diskRadius = 0.005f;// (1.0f + (length(u_ViewPosition - position) / u_FarPlane)) / 25.0f;
-
-        for (int i = 0; i < 20; ++i) 
+        float offset = 0.01f;
+        float delta = (2.0f * offset) / u_FilterSize;
+    
+        for (float i = -offset; i < offset; i += delta)
         {
-            float nearest = texture(u_PointLight.ShadowMap, -light.xyz + sampleOffsetDirections[i] * diskRadius).r * u_FarPlane;
-            if (nearest + bias < distance)  
+            for (float j = -offset; j < offset; j += delta)
             {
-                shadowIntensity += 1;
-            }
-        }
-
-        shadowIntensity /= 20;
-        */
-        if (u_PointLight.UseShadowMap)
-        {
-            float offset = 0.01f;
-            float delta = (2.0f * offset) / u_FilterSize;
-
-            for (float i = -offset; i < offset; i += delta)
-            {
-                for (float j = -offset; j < offset; j += delta)
+                for (float k = -offset; k < offset; k += delta)
                 {
-                    for (float k = -offset; k < offset; k += delta)
+                    float nearest = texture(u_PointLight.ShadowMap, -light.xyz + vec3(i, j, k)).r * u_FarPlane;
+                    if (nearest + bias < distance)
                     {
-                        float nearest = texture(u_PointLight.ShadowMap, -light.xyz + vec3(i, j, k)).r * u_FarPlane;
-                        if (nearest + bias < distance)
-                        {
-                            shadowIntensity += 1;
-                        }
+                        shadowIntensity += 1;
                     }
                 }
             }
-        
-            shadowIntensity /= u_FilterSize * u_FilterSize * u_FilterSize;
         }
-        /*
-        float fsize2 = u_FilterSize * u_FilterSize;
-        for (float j = 0; j < 3; ++j) 
-        {
-            for (float i = 0; i < fsize2; ++i) 
-            {
-                vec3 offset = texture2D(u_RandomSamples, vec2(j / 10, i / fsize2)).xyz * u_ShadowMapPixelSize / 2;
-                float nearest = texture(u_PointLight.ShadowMap, -light.xyz + offset).r * u_FarPlane;
-                if (nearest + bias < distance)  
-                {
-                    shadowIntensity += 1;
-                }
-            }
-        }
+    
+        shadowIntensity /= u_FilterSize * u_FilterSize * u_FilterSize;
 
-        shadowIntensity /= fsize2 * 3;
-        */
-
-        color = vec4((1.0f - shadowIntensity) * (diffuse + specular) * radiance * NdotL, 1.0f);
-        //color = vec4((1.0f - shadowIntensity), (1.0f - shadowIntensity), shadowIntensity, 1.0f);
+        return 1.0f - shadowIntensity;
     }
     else
     {
-        color = vec4(albedo, 1.0f);
+        return 1.0f;
     }
+}
+
+vec3 GetIntensity(vec2 pos, vec3 normal, vec3 view, vec3 light)
+{
+    vec3 albedo = texture(u_Albedo, pos).xyz;
+
+    vec4 roughnessMetalic = texture(u_RoughnessMetalic, pos);
+    float roughness = roughnessMetalic.x;
+    float metalic = roughnessMetalic.y;
+
+    vec3 h = normalize(light + view);
+
+    float NdotV = max(dot(normal, view), 0.0f);
+    float HdotV = max(dot(h, view), 0.0f);
+    float NdotH = max(dot(normal, h), 0.0f);
+    float NdotL = max(dot(normal, light), 0.0f);
+    float LdotH = max(dot(light, h), 0.0f);
+    
+    float NdotH2 = NdotH * NdotH;
+    float r2 = roughness * roughness;
+
+    vec3 F0 = mix(vec3(0.04f), albedo, metalic);
+
+    vec3 F = F0 + (vec3(1.0f) - F0) * pow(clamp((1.0f - HdotV), 0.0f, 1.0f), 5.0f);
+    
+    float denominator = (NdotH2 * (r2 * r2 - 1.0f) + 1.0f);
+    float D = r2 * r2 / (M_PI * denominator * denominator + 0.0001f); // r2 * r2 is Epic stuff ;)
+    
+    float G = GX(NdotV, roughness) * GX(NdotL, roughness);
+    
+    vec3 specular = F * G * D / (4.0f * NdotV * NdotL + 0.0001f);
+    vec3 diffuse = (vec3(1.0f) - F) * albedo / M_PI;
+
+    return (diffuse + specular) * NdotL;
+}
+
+vec3 GetAttenuation(vec2 pos, vec3 position)
+{
+    float distance = length(u_PointLight.Position - position);
+    return u_PointLight.Intensity * u_PointLight.Color / (distance * distance);
+}
+
+void main()
+{
+    vec2 pos = gl_FragCoord.xy / u_ScreenSize;
+    
+    vec3 position = texture(u_Position, pos).xyz;
+    vec3 normal = texture(u_Normal, pos).xyz;
+    
+    vec3 view = normalize(u_ViewPosition - position);
+    vec3 light = normalize(u_PointLight.Position - position);
+
+    float visibility = GetVisibility(pos, light, normal, position);
+    vec3 intensity = GetIntensity(pos, normal, view, light);
+    vec3 attenuation = GetAttenuation(pos, position);
+
+    color = vec4(visibility * intensity * attenuation, 1.0f);
 }
